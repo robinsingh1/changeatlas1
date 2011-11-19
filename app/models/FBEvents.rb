fe77f710e51e825793a18bf1a9ad8cb3b652e0c3
Santiago Gil <gilcastdev@gmail.com>
2011-11-19 04:00:25 -0500

    FBEvents

---
+++ app/models/FBEvents.rb
@@ -0,0 +1,61 @@
+require 'rubygems'
+require 'json'
+require "net/http"
+
+class FBEvent 
+  attr_accessor :url
+  
+  def initialize(url)
+    id = url.split("/").last 
+    newurl = "http://graph.facebook.com/#{id}/"  
+    @resp = Net::HTTP.get_response(URI.parse(newurl))
+    @result = JSON.parse(@resp.body)
+    out = "true"
+  end
+  def getJSON
+    evJSON = @resp
+  end
+  def getEndTime
+    evEnd = "#{@result["end_time"]}"
+  end
+  def getLastUpdate
+    evLastUp = "#{@result["updated_time"]}"
+  end
+  def getOwnerID 
+    evOwnID = "#{@result["owner"]["id"]}"
+  end
+  def getEventID
+    evID = "#{@result["id"]}"
+  end
+  def getEventName 
+    evName = "#{@result["name"]}"
+  end 
+  def getEventLocation  
+    evLoc = "#{@result["location"]}"
+  end
+  def getEventLatitude
+    evVen = @result["venue"]
+    if evVen.nil?
+      evLat = "No Latitude"
+    else
+      evLat = "#{@result["venue"]["latitude"]}"
+    end
+  end 
+  def getEventLongitude 
+    evVen = @result["venue"]
+    if evVen.nil?
+      evLong = "No Longitude"
+    else
+      evLong = "#{@result["venue"]["longitude"]}"
+    end
+  end
+  def getEventDescription 
+    evDes = @result["description"]
+    if evDes.nil?
+      evLong = "No Description"
+    else
+      evLong = "#{@result["description"]}"
+    end
+  end
+end
+
