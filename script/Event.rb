b602d7ff431ad7467c2e3476795e5cde89ef7bae
Santiago Gil <gilcastdev@gmail.com>
2011-11-19 01:45:55 -0500

    Added Event.rb to /script

---
+++ script/Event.rb
@@ -0,0 +1,60 @@
+require 'rubygems'
+require 'json'
+require "net/http"
+
+class Event
+  attr_accessor :url
+  
+  def initialize(url)
+    id = url.split("/").last 
+    newurl = "http://graph.facebook.com/#{id}/"  
+    @resp = Net::HTTP.get_response(URI.parse(newurl))
+    @result = JSON.parse(@resp.body)
+  end
+  def getJSON
+    evJSON = @resp
+  end
+  def getEventID
+    evID = "#{@result["id"]}"
+  end
+  def getEventName 
+    evName = "#{@result["name"]}"
+  end 
+  def getEventLocation  
+    evLoc = "#{@result["location"]}"
+  end
+  def getEventLatitude
+    evVen = @result["venue"]
+    if evVen.nil?
+      evLat = "No Latitude"
+    else
+      evLat = "#{@result["venue"]["latitude"]}"
+    end
+  end 
+  def getEventLongitude 
+    evVen = @result["venue"]
+    if evVen.nil?
+      evLong = "No Longitude"
+    else
+      evLong = "#{@result["venue"]["longitude"]}"
+    end
+  end
+  def getEventDescription 
+    evDes = @result["description"]
+    if evDes.nil?
+      evLong = "No Description"
+    else
+      evLong = "#{@result["description"]}"
+    end
+  end
+end
+
+
+ev = Event.new("https://www.facebook.com/events/302836383073987/")
+puts ev.getJSON
+puts ev.getEventID
+puts ev.getEventName
+puts ev.getEventLocation
+puts ev.getEventLatitude
+puts ev.getEventLongitude
+puts ev.getEventDescription
