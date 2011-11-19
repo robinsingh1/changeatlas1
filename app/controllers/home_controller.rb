481b75d54350cc8eeb290fe82539910481919f44
Natasha DSouza <natasha@xtremelabs.com>
2011-11-19 05:30:14 -0500

    Gen unique id

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,9 @@ class HomeController < ApplicationController
     @locations = Event.all
     @json = Event.all.to_gmaps4rails
   end
+  
+  def events
+    @event = Event.find_by_eventID(params[:eventID]).to_gmaps4rails
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 ChangeAtlas::Application.routes.draw do
   get "home/index"
+  get "home/events"
+  
+  root :to => "home#index"
+  
+  match 'events/:eventID', :to => 'home#events', :as => 'events'
+  
+  
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,10 +58,8 @@ ChangeAtlas::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
   
-  match 'FBEvent#initialize', :to => "FBEvent#initialize", :as => "initializeEvent"
-
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
