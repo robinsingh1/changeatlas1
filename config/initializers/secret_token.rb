851d80c6ac4d4330a44546c60883cfa51c926396
Ubuntu <ubuntu@ip-10-124-229-195.ec2.internal>
2011-11-19 04:37:52 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Changeatlas1::Application.config.secret_token = 'becc2e5a9e1c1fe3a83d3313f30328ba77a3c5148719b28b3dfa5dd4860e242ef6e215b14a9f6072ac81c2f4c1f84b51ff6f8980fcd77e1cc6bbbf4f1bcbe254'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Changeatlas1::Application.config.session_store :cookie_store, key: '_changeatlas1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Changeatlas1::Application.config.session_store :active_record_store
