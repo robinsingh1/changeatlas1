106ae898d0606ac18c5d743332e3f64663876569
Natasha DSouza <natasha@xtremelabs.com>
2011-11-19 00:52:37 -0500

    Working marker and finding location

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,6 @@ class HomeController < ApplicationController
   def index
     @locations = Event.all
     @json = Event.all.to_gmaps4rails
-    @markers = '[{"description": "", "title": "", "sidebar": "", "lng": "-79.381961", "lat": "43.652527", "picture": "rails.png", "width": "10px", "height": "10px"},
-                {"lng": "-79.381961", "lat": "43.652527" }]'
   end
 
 end
